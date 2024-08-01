0b88e0f6c118a42ccd91c7c04b439acc4b3a7f55
toufique-imam <2013nuhash@gmail.com>
2024-08-02 01:44:40 +0600

    up

--- vite.config.ts
+++ vite.config.ts
@@ -4,12 +4,6 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), nodePolyfills({
-    globals: {
-      // Don't polyfill these globals
-      process: false,
-      Buffer: false,
-    },
-  }),],
-  base: '/ton-counter-frontend/',
+  plugins: [react(), nodePolyfills()],
+  base: "/first_contract_front_end/",
 });
