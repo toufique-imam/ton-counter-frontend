9cc80eea62b3c44dc2d8fa44690c87a81dd8f188
toufique-imam <2013nuhash@gmail.com>
2024-08-04 22:02:54 +0600

    test

--- src/App.tsx
+++ src/App.tsx
@@ -29,6 +29,8 @@ function App() {
       <div>
         <div className='Card'>
           <b>{WebApp.platform}</b>
+          <br/>
+          <b>{JSON.stringify(WebApp.initDataUnsafe)}</b>
           <b>Our contract Address</b>
           <div className='Hint'>{contract_address?.slice(0, 30) + "..."}</div>
           <b>Our contract Balance</b>
